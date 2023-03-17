d016e6142f6fa096fee8a90871d7f1f25f5a885b
danielphiri <danielphiri@berkeley.edu>
2023-03-17 11:29:42 -0700

    added social media icons

--- src/@lekoarts/gatsby-theme-jodie/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-jodie/components/footer.tsx
@@ -2,6 +2,14 @@
 import { jsx, Link, useThemeUI, get } from "theme-ui"
 import { readableColor } from "polished"
 import useSiteMetadata from "@lekoarts/gatsby-theme-jodie/src/hooks/use-site-metadata"
+import { Facebook, Twitter, Linkedin, Pocket, Mail } from 'react-feather';
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  PocketShareButton,
+  EmailShareButton,
+} from 'react-share';
 
 const Footer = ({ bg }: { bg: string }) => {
   const { siteTitle } = useSiteMetadata()
@@ -53,6 +61,23 @@ const Footer = ({ bg }: { bg: string }) => {
         >
           Daniel
         </Link>
+        <br></br>
+      <FacebookShareButton url={"https"} quote={"FB"}>
+        <Facebook strokeWidth={1.25} />
+      </FacebookShareButton>
+      &nbsp;&nbsp;
+      <LinkedinShareButton url={"url"} title={"Welcome"} summary={"Try Dis"}>
+        <Linkedin strokeWidth={1.25} />
+      </LinkedinShareButton>
+      &nbsp;&nbsp;
+      <TwitterShareButton url={"Hoa"} title={"Okay"}>
+        <Twitter strokeWidth={1.25} />
+      </TwitterShareButton>
+      &nbsp;&nbsp;
+      <EmailShareButton url={"wow"} title={"greatness"}>
+        <Mail strokeWidth={1.25} />
+      </EmailShareButton>
+
       </div>
     </footer>
   )
