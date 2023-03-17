052aab9c4018582cb66fbc23759a9c4c0cb2f88a
danielphiri <danielphiri@berkeley.edu>
2023-03-16 18:16:30 -0700

    updated theme colors

--- src/gatsby-plugin-theme-ui/index.ts
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -5,6 +5,11 @@ const theme = merge(originalTheme, {
   fonts: {
     body: `"Work Sans", -apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"`,
   },
+  colors: {
+    text: '#fff',
+    background: '#000',
+    primary: '#33e',
+  },
 })
 
 export default theme
